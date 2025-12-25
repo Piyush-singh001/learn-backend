2f9fc111f7d6aea2ebe3b22f6404289628471354
Piyush-singh001 <piyushsengar243@gmail.com>
2025-12-25 23:56:28 +0530

    learn how to create server using express

--- express/index.js
+++ express/index.js
@@ -1,17 +1,16 @@
-const http = require("http");
 const express = require("express");
 const app = express();
 
-app.use((req, res, next) => {
+app.use("/", (req, res, next) => {
   console.log(req.method, req.url);
-  res.end("Hello from Express server");
-  
+  res.send("<h1>Hello from Express server</h1>");
   next();
 });
-app.use((req, res, next) => {
+app.use("/hello", (req, res, next) => {
   console.log("This is the second middleware");
+  res.send("<p>Hello from /hello route</p>");
 });
 const PORT = 3000;
-http.createServer(app).listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
