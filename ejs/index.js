ba4c06217ca527e273c5635deb6b3c85ce220752
Piyush-singh001 <piyushsengar243@gmail.com>
2025-12-27 00:00:19 +0530

    add style

--- ejs/index.js
+++ ejs/index.js
@@ -8,6 +8,7 @@ const path = require("path");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded());
 app.use(UserRouter);
 app.use(HostRouter);
