ead47f5f2bb18b1549eb99f9ebc9bf7cd94c62a0
Piyush-singh001 <piyushsengar243@gmail.com>
2025-12-27 23:53:49 +0530

    edit app.js

--- ejs/app.js
+++ ejs/app.js
@@ -1,1 +1,5 @@
 console.log("ejs app.js");
+const express = require("express");
+const app = express();
+const UserRouter = require("./routes/userRouter");
+const HostRouter = require("./routes/hostRouter");
