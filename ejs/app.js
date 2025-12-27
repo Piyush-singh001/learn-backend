3505ff1eae3148c458ada316c9515f0da7366372
Piyush-singh001 <piyushsengar243@gmail.com>
2025-12-27 23:54:48 +0530

    delete app.js

--- ejs/app.js
+++
@@ -1,5 +0,0 @@
-console.log("ejs app.js");
-const express = require("express");
-const app = express();
-const UserRouter = require("./routes/userRouter");
-const HostRouter = require("./routes/hostRouter");
